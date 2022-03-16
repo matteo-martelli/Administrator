2d1e96d37a3153f0e47fca8e60b51310319b08fe
matteo-martelli <matteo.martelli1@studenti.unimi.it>
2022-03-16 12:25:17 +0100

    metodi

---
+++ source/repos/ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+namespace metodiImmagini;
+
+public class metodi
+{
+    public StreamReader Read = new StreamReader("reference_le.pfm");
+    public bool magic()
+    {
+        if(Read.Readline() == "PF")
+            return true;
+        else 
+            return false;   
+    }
+    public int[] res()
+    {
+        string temp_resolution = Read.Readline();
+        string[] r =line.Split(' ');
+        int[] resolution = new int[2];
+        resolution[0] = int.Parse(r[0]);
+        resolution[1] = int.Parse(r[1]);
+        return resolution;
+    }
+}
